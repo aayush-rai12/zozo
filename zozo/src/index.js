55d7b4b1cc189f3abf80d5872e456787b49d8981
aayush-rai12 <ayush.wrk1212@gmail.com>
2024-05-02 00:26:14 +0530

    main code login and register component

--- zozo/src/index.js
+++ zozo/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './components/login';
+import Login from './components/login/login';
+import Register from './components/register/Registration';
+// import Home from './components/Home/Home'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Login />
+    <Register />
+    {/* <Home /> */}
   </React.StrictMode>
 );
