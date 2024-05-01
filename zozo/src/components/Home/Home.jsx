e1b8aa8749a6f83f916fcbf0191bf33ab87960c3
aayush-rai12 <ayush.wrk1212@gmail.com>
2024-05-02 00:36:09 +0530

    code commit for login and register page

---
+++ zozo/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// const CustomButton = () => {
+
+//   return (
+//     <h1>
+//         Press me
+//     </h1>
+
+//   );
+// };
+
+// export default CustomButton;
