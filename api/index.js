bdbc20d4eb270521aaada5bd188a951c49ff93f7
aayush-rai12 <ayush.wrk1212@gmail.com>
2024-08-08 23:30:53 +0530

    ZOZO-9:committed the code changes , setup mongo and express and other stuff

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+import userRouter from './routes/user.route.js';
+// Configure dotenv
+dotenv.config();
+
+// Connect to MongoDB
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} !!!!!`);
+});
+
+app.use('/api/user',userRouter);
